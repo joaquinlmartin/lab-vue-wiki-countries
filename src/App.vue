bd11327b0c633728b5ddbadff4e22c94a94a8329
unknown <joaquinlmartin@gmail.com>
2022-09-28 21:08:03 +0200

    iteration 0, 1 and 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="app">
+		<Navbar class="navbar-menu" />
+	</div>
+	<div class="showThing">
+		<div class="country-list">
+			<CountriesList :countries="getCountry" />
+		</div>
+		<div class="country-detail">
+			<router-view></router-view>
+		</div>
+	</div>
 </template>
 
+<script>
+import countriesData from '../public/countries.json';
+import Navbar from './components/Navbar.vue';
+import CountriesList from './components/CountriesList.vue';
+
+export default {
+	name: 'App',
+	components: { Navbar, CountriesList },
+	data() {
+		return {
+			countries: countriesData,
+		};
+	},
+	computed: {
+		getCountry() {
+			return this.countries.map((country) => {
+				let countryId = 0;
+				return {
+					id: countryId++,
+					name: country.name.common,
+					alpha2Code: country.alpha2Code,
+					alpha3Code: country.alpha3Code,
+				};
+			});
+		},
+	},
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+	.showThing {
+		display: flex;
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+/* import CountryDetails from './views/CountryDetails.vue'; */
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+	{
+		path: '/',
+		name: 'root',
+		component: HomeView,
+	},
+	/* {
+    path: '/:countryCode',
+    name: 'CountryDetails',
+    component: CountryDetails,
+
+  }, */
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory('/'),
+	routes,
+	scrollBehavior() {
+		document.getElementById('app').scrollIntoView();
+	},
 });
 
 export default router;
